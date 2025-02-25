cc2d6f29b309aaef07b57ba6a07a3d61a34e8aec
MELEGHOST <kosbek043@gmail.com>
2025-02-26 02:59:52 +0300

    Create top.js

---
+++ pages/top.js
@@ -0,0 +1,10 @@
+const Top = () => {
+  return (
+    <div className="frame top">
+      <h2>Топ Стримеров</h2>
+      <p>Топ стримеров обновляется...</p>
+    </div>
+  );
+};
+
+export default Top;
