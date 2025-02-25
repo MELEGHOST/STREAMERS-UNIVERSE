895e4736fe80a0771efe6d07473aaa414abc2ed0
MELEGHOST <kosbek043@gmail.com>
2025-02-26 02:59:28 +0300

    Create twitch.js

---
+++ pages/twitch.js
@@ -0,0 +1,10 @@
+const Twitch = () => {
+  return (
+    <div className="frame twitch">
+      <h2>Twitch Трекер</h2>
+      <p>Twitch трекер в разработке...</p>
+    </div>
+  );
+};
+
+export default Twitch;
