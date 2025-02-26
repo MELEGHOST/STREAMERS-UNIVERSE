66079856fef9837f96dfc15b2044dfab81b444a9
MELEGHOST <kosbek043@gmail.com>
2025-02-26 04:42:23 +0300

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Stars from './Stars';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img src="/logo.png" alt="Streamers Universe Logo" className="logo" />
+      </div>
+      {children}
+      <Stars />
+    </div>
+  );
+};
+
+export default Layout;
