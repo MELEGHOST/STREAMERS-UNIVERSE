27ea4ee64e774d298a405ce4952ffb9275d4f6c5
MELEGHOST <kosbek043@gmail.com>
2025-02-26 04:28:08 +0300

    Update twitch.js

--- pages/twitch.js
+++ pages/twitch.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { useAuth } from '../src/context/AuthContext';
+import { useRouter } from 'next/router';
+import Menu from '../src/components/Menu';
+import Stars from '../src/components/Stars';
+
 const Twitch = () => {
+  const { currentUser } = useAuth();
+  const router = useRouter();
+
+  if (typeof window !== 'undefined' && !currentUser) {
+    router.push('/auth');
+    return null;
+  }
+
   return (
-    <div className="frame twitch">
-      <h2>Twitch Трекер</h2>
-      <p>Twitch трекер в разработке...</p>
+    <div className="container">
+      <div className="logo-container">
+        <img src="/logo.png" alt="Streamers Universe Logo" className="logo" />
+      </div>
+      <Menu />
+      <div className="frame twitch active">
+        <h2>Twitch Трекер</h2>
+        <p>Twitch трекер в разработке...</p>
+      </div>
+      <Stars />
     </div>
   );
 };
