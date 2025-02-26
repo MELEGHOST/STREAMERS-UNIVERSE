ab030cad6082a12f229a9225dd5887751353aba5
MELEGHOST <kosbek043@gmail.com>
2025-02-26 04:54:01 +0300

    Update twitch.js

--- pages/twitch.js
+++ pages/twitch.js
@@ -1,31 +1,22 @@
+'use client';
+
+import dynamic from 'next/dynamic';
 import React from 'react';
-import { useAuth } from '../src/context/AuthContext';
-import { useRouter } from 'next/router';
-import Menu from '../src/components/Menu';
-import Stars from '../src/components/Stars';
+import Layout from '../src/components/Layout';
 
+// Динамически импортируем компонент Twitch для клиентского рендеринга
 const Twitch = () => {
-  const { currentUser } = useAuth();
-  const router = useRouter();
-
-  if (typeof window !== 'undefined' && !currentUser) {
-    router.push('/auth');
-    return null;
-  }
-
   return (
-    <div className="container">
-      <div className="logo-container">
-        <img src="/logo.png" alt="Streamers Universe Logo" className="logo" />
-      </div>
-      <Menu />
-      <div className="frame twitch active">
+    <Layout>
+      <div className="frame twitch">
         <h2>Twitch Трекер</h2>
         <p>Twitch трекер в разработке...</p>
       </div>
-      <Stars />
-    </div>
+    </Layout>
   );
 };
 
-export default Twitch;
+// Экспортируем динамически загружаемый компонент
+export default dynamic(() => Promise.resolve(Twitch), {
+  ssr: false, // Отключаем серверный рендеринг (SSG/SSR)
+});
