136ccb28eccb17ac6d0597fde8be7900fd6f5587
MELEGHOST <kosbek043@gmail.com>
2025-02-26 03:32:21 +0300

    Update auth.js

--- pages/auth.js
+++ pages/auth.js
@@ -1,10 +1,18 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TwitchAuth from '../src/components/TwitchAuth';
 import Stars from '../src/components/Stars';
 
 const Auth = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null; // Предотвращаем рендеринг до загрузки клиента
+
   return (
     <div className="container">
       <TwitchAuth />
