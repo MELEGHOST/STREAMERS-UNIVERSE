617e74d7376d1af1758b9f2d1f5a8577c952c69e
MELEGHOST <kosbek043@gmail.com>
2025-02-27 04:10:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import { AuthProvider } from '../src/context/AuthContext';
 
-function MyApp({ Component, pageProps }) {
-  return _jsx(AuthProvider, {
-    children: _jsx(Component, { ...pageProps })
-  });
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
-
-export default MyApp;
