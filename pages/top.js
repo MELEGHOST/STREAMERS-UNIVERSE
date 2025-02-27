37df3c82e1103b084905f64383ee051b10a59a2e
MELEGHOST <kosbek043@gmail.com>
2025-02-27 16:51:44 +0300

    Update top.js

--- pages/top.js
+++ pages/top.js
@@ -1,9 +1,7 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import dynamic from 'next/dynamic';
-import React from 'react';
+const React = require('react');
 
-function TopPage() {
-  return _jsxs("div", { children: [_jsx("h1", { children: "Топ" }), _jsx("p", { children: "Список топовых стримеров" })] });
+function Top() {
+  return React.createElement('div', null, React.createElement('h1', null, 'Топ'));
 }
 
-export default TopPage;
+module.exports = Top;
