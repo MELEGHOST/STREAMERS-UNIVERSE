81b7179fe44c1367c474a3d8c387d5bf6332331f
MELEGHOST <kosbek043@gmail.com>
2025-02-27 16:51:01 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 const React = require('react');
+const { AuthProvider } = require('../src/context/AuthContext');
 
 function MyApp({ Component, pageProps }) {
-  return React.createElement(Component, pageProps);
+  return React.createElement(AuthProvider, null, React.createElement(Component, pageProps));
 }
 
 module.exports = MyApp;
