917f8526d9a62603bcd1357335c3f0cbb2402bfd
MELEGHOST <kosbek043@gmail.com>
2025-02-27 03:40:17 +0300

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,24 +1,10 @@
 // Страница профиля пользователя
-'use client';
-
+import { jsx as _jsx } from "react/jsx-runtime";
 import dynamic from 'next/dynamic';
 import React from 'react';
-import Layout from '../src/components/Layout'; // Исправленный путь к Layout
-import Profile from '../src/components/Profile'; // Убедимся, что Profile также импортируется корректно
 
-// Динамически импортируем компонент ProfilePage для клиентского рендеринга
-const ProfilePage = () => {
-  console.log('Rendering ProfilePage'); // Отладка
-  // Возвращаем layout с компонентом профиля
-  return (
-    <Layout>
-      <Profile />
-    </Layout>
-  );
-};
+function ProfilePage() {
+  return _jsx("div", { children: "Профиль пользователя" });
+}
 
-// Экспортируем динамически загружаемый компонент с правильной конфигурацией
-export default dynamic(() => Promise.resolve(ProfilePage), {
-  ssr: false, // Отключаем серверный рендеринг (SSG/SSR)
-  loading: () => <div>Загрузка профиля...</div>, // Добавляем индикатор загрузки
-});
+export default ProfilePage;
