584263c5fec3fe84dea1271a21f39ca74900a246
MELEGHOST <kosbek043@gmail.com>
2025-02-27 16:53:28 +0300

    Update callback.js

--- pages/api/auth/twitch/callback.js
+++ pages/api/auth/twitch/callback.js
@@ -2,7 +2,6 @@ async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-  // Логика обмена кода на токен Twitch
   res.status(200).json({ token: 'example_token' });
 }
 
