d4c6db8c330277ec680c155ea59135f0d3c5d33a
MELEGHOST <kosbek043@gmail.com>
2025-02-27 16:52:57 +0300

    Update logout.js

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,8 +1,7 @@
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-  // Логика выхода (например, очистка сессии)
   res.status(200).json({ message: 'Logged out successfully' });
 }
 
