73c7b8b5ea25e42e5172a4c40ac66c6e2028ae54
MELEGHOST <kosbek043@gmail.com>
2025-02-27 04:30:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,39 @@
+const { withCloudflare } = require('@cloudflare/next-on-pages');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: 'standalone',
   env: {
     TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-    TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI || "https://streamers-universe.pages.dev/auth",
+    TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI || 'https://streamers-universe.pages.dev/auth',
     TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET
   },
   images: {
-    domains: ["id.twitch.tv", "api.twitch.tv"],
+    domains: ['id.twitch.tv', 'api.twitch.tv'],
     unoptimized: true
   },
   webpack: (config) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false, net: false, tls: false };
+    // Добавляем правило для файлов .js в папке pages
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /pages/,
+      type: 'javascript/esm', // Указываем тип модуля как ES-модули
+    });
+
+    // Отключаем fallback для модулей, которые не нужны в браузере
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false
+    };
+
     return config;
   },
   experimental: {
-    turbo: true,
-    optimizePackageImports: ["react", "react-dom"]
+    turbo: {}
   }
 };
 
-module.exports = nextConfig;
+module.exports = withCloudflare(nextConfig);
