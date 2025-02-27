d1219907491cfa4509967ede910e2e526422baca
MELEGHOST <kosbek043@gmail.com>
2025-02-27 16:53:10 +0300

    Update me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -2,7 +2,6 @@ async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-  // Логика получения данных пользователя
   res.status(200).json({ user: 'example_user' });
 }
 
