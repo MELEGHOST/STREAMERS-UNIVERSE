557dbcb9ed52bf19204eb84a9b25fb2a2e203466
MELEGHOST <kosbek043@gmail.com>
2025-02-28 03:22:48 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 const React = require('react');
+const { useAuth } = require('../src/context/AuthContext');
 
 function Home() {
-  return React.createElement('h1', null, 'Welcome to Streamers Universe');
+  const { isAuthenticated, getGreeting } = useAuth();
+
+  return React.createElement('div', null, [
+    React.createElement('h1', null, `${getGreeting()}, Welcome to Streamers Universe!`),
+    !isAuthenticated && React.createElement('a', { href: '/auth' }, 'Login with Twitch'),
+    isAuthenticated && React.createElement('a', { href: '/profile' }, 'Go to Profile')
+  ]);
 }
 
 module.exports = Home;
