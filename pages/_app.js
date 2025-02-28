9d36f16f50b4f9f50507242356aa6d054914b592
MELEGHOST <kosbek043@gmail.com>
2025-03-01 01:00:49 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-const React = require('react');
-const { AuthProvider } = require('../src/context/AuthContext');
+import { AuthProvider } from '../src/context/AuthContext';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
-  return React.createElement(AuthProvider, null, React.createElement(Component, pageProps));
+export default function App({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </SessionProvider>
+  );
 }
-
-module.exports = MyApp;
