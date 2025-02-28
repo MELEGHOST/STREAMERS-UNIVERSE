10ae5385545bb1fd03b7309ada61d1deb7dffd82
MELEGHOST <kosbek043@gmail.com>
2025-02-28 03:24:02 +0300

    Create twitch.js

---
+++ pages/api/auth/twitch.js
@@ -0,0 +1,12 @@
+const React = require('react');
+
+async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.TWITCH_CLIENT_ID}&redirect_uri=${process.env.TWITCH_REDIRECT_URI}&response_type=code&scope=user:read:email+user:read:follows`;
+  res.status(200).json({ url: twitchAuthUrl });
+}
+
+module.exports = handler;
