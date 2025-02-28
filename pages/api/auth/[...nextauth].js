1f5f50fab2a07b131ab9547e8071194261e2ba0b
MELEGHOST <kosbek043@gmail.com>
2025-03-01 00:36:00 +0300

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,23 @@
+import NextAuth from 'next-auth';
+import TwitchProvider from 'next-auth/providers/twitch';
+
+export default NextAuth({
+  providers: [
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+});
