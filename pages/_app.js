46014782eb27942d51d09ab5c40ee202934fb2d9
MELEGHOST <kosbek043@gmail.com>
2025-03-01 01:04:12 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
