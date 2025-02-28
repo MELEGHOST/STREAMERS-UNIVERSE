56b1f7564c04e897a1c88c93fc8c778ed80ab33e
MELEGHOST <kosbek043@gmail.com>
2025-02-28 03:32:14 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 const React = require('react');
 const { AuthProvider } = require('../src/context/AuthContext');
+const styled = require('styled-components').default;
+
+const Container = styled.div`
+  background-color: #f5f5f5;
+  min-height: 100vh;
+  padding: 20px;
+  font-family: 'Arial', sans-serif;
+`;
+
+const Logo = styled.img`
+  max-width: 200px;
+  margin-bottom: 20px;
+`;
 
 function MyApp({ Component, pageProps }) {
-  return React.createElement(AuthProvider, null, React.createElement(Component, pageProps));
+  return React.createElement(
+    Container,
+    null,
+    [
+      React.createElement(Logo, { src: '/logo.png', alt: 'Streamers Universe Logo' }),
+      React.createElement(AuthProvider, null, React.createElement(Component, pageProps))
+    ]
+  );
 }
 
 module.exports = MyApp;
