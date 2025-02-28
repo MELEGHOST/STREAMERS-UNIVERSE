c27ef13b49d19313533c973de140832bcc53974f
MELEGHOST <kosbek043@gmail.com>
2025-02-28 18:21:00 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,9 @@
-const React = require('react');
-const { AuthProvider } = require('../src/context/AuthContext');
-const styled = require('styled-components').default;
+import { AuthProvider } from '../src/context/AuthContext';
 
-const Container = styled.div`
-  background-color: #f5f5f5;
-  min-height: 100vh;
-  padding: 20px;
-  font-family: 'Arial', sans-serif;
-`;
-
-const Logo = styled.img`
-  max-width: 200px;
-  margin-bottom: 20px;
-`;
-
-function MyApp({ Component, pageProps }) {
-  return React.createElement(
-    Container,
-    null,
-    [
-      React.createElement(Logo, { src: '/logo.png', alt: 'Streamers Universe Logo' }),
-      React.createElement(AuthProvider, null, React.createElement(Component, pageProps))
-    ]
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
-
-module.exports = MyApp;
