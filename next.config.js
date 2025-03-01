f552c9fd86efa6d43735a8f120026d81f699c40b
MELEGHOST <kosbek043@gmail.com>
2025-03-01 04:02:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,17 @@ export default {
   reactStrictMode: true,
   output: "standalone",
   env: {
-    TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-    TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI || "https://streamers-universe.vercel.app/auth",
-    TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET
+    TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI || "https://streamers-universe.vercel.app/auth" // Только публичные данные
   },
   images: {
     domains: ["id.twitch.tv", "api.twitch.tv"],
     unoptimized: true
   },
   experimental: {
-    esmExternals: true // Включаем поддержку ES-модулей для внешних зависимостей
+    esmExternals: true
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
   }
 };
