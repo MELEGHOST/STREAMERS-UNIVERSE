14b489783c56e01ac08a17e435828b27a66e62b4
MELEGHOST <kosbek043@gmail.com>
2025-03-02 04:18:30 +0300

    Update me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -1,10 +1,13 @@
-import { getServerSession } from 'next-auth/next';
+import { getServerSession } from 'next-auth';
 import { authOptions } from './[...nextauth]';
 
 export default async function handler(req, res) {
   try {
+    console.log('Processing /api/auth/me request...'); // Отладка
     const session = await getServerSession(req, res, authOptions);
     
+    console.log('Session retrieved:', session); // Отладка
+
     if (!session) {
       return res.status(401).json({ message: 'Не авторизован' });
     }
