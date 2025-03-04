b687443608b91a2bca4d584e6363610930d41341
MELEGHOST <kosbek043@gmail.com>
2025-03-04 18:29:22 +0300

    Update me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { getAuthOptions } from './[...nextauth]';
+import { getAuthOptions } from '../auth/[...nextauth]';
 
 export default async function handler(req, res) {
   try {
