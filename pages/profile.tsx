e9d12ca7d4113de3fb560990d3be5a1fc34cfd24
MELEGHOST <kosbek043@gmail.com>
2025-03-05 02:07:02 +0300

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useSession } from 'next-auth/client';
+import { useSession } from 'next-auth/react'; // Обновленный импорт
 
 const Profile = () => {
   const [session, loading] = useSession();
