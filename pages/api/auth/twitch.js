7bbf5a9f901ce4fab823072a96968191f0216183
MELEGHOST <kosbek043@gmail.com>
2025-03-04 18:53:10 +0300

    Update twitch.js

--- pages/api/auth/twitch.js
+++ pages/api/auth/twitch.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { getAuthOptions } from './[...nextauth]';
+import { getAuthOptions } from '../auth/[...nextauth]'; // Убедимся, что путь корректен
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
