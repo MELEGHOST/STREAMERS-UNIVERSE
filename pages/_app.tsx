7a93c158566f45e1a02b355795ef91a49b289505
MELEGHOST <kosbek043@gmail.com>
2025-03-05 02:58:37 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AppProps } from 'next/app';
-import '../styles/global.css'; // Исправлено с globals.css на global.css
+import '../../styles/global.css'; // Исправлен путь с ../styles/globals.css на ../../styles/global.css
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
