a2128229b8a853eaf452a5136414dad7195ae06d
MELEGHOST <kosbek043@gmail.com>
2025-03-04 18:30:17 +0300

    Update socials.js

--- pages/api/socials.js
+++ pages/api/socials.js
@@ -1,6 +1,6 @@
 import { Pool } from '@vercel/postgres';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from './[...nextauth]';
+import { getAuthOptions } from '../auth/[...nextauth]';
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const session = await getServerSession(req, res, authOptions);
+  const session = await getServerSession(req, res, getAuthOptions());
   if (!session) {
     return res.status(401).json({ error: 'Not authenticated' });
   }
