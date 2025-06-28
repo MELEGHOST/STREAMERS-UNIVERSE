7644e8dae1dca17ba137b8ffbce46d0687bcabc6
MELEGHOST <kosbek043@gmail.com>
2025-06-28 18:23:15 +0300

    Восстановлен компонент StyledComponentsRegistry в layout.js для корректного управления стилями. Удален компонент AuthProvider в providers.js, заменен на фрагмент. Все изменения проверены на наличие ошибок и соответствие функционалу.

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import '../i18n';
 
 import '../styles/globals.css';
 import { Providers } from './providers';
-// import StyledComponentsRegistry from './lib/registry';
+import StyledComponentsRegistry from './lib/registry';
 
 export const metadata = {
   title: 'Streamers Universe',
@@ -13,13 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body>
-        {/* <StyledComponentsRegistry> */}
+        <StyledComponentsRegistry>
           <Providers>
             <main>
               {children}
             </main>
           </Providers>
-        {/* </StyledComponentsRegistry> */}
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
