042190fd43eedb6ee246c50f97a434c2d874ee5c
MELEGHOST <kosbek043@gmail.com>
2025-06-29 02:11:46 +0300

    fix: Перемещен импорт i18n в I18nProvider для улучшения структуры кода и обеспечения корректной работы локализаций

--- app/components/I18nProvider.js
+++ app/components/I18nProvider.js
@@ -1,8 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import i18n from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
 import en from '../../public/locales/en/common.json';
 import ru from '../../public/locales/ru/common.json';
 import uk from '../../public/locales/uk/common.json';
