8f7b5a6c3246a2e150d5abaf9e49c8cbe7d17bff
MELEGHOST <kosbek043@gmail.com>
2025-06-28 17:33:46 +0300

    Добавлено свойство enableStyleSheetManager в компонент StyledComponentsRegistry для улучшения управления стилями. Все изменения проверены на наличие ошибок и соответствие функционалу.

---
+++ app/reviews/create/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import styles from './create-review.module.css';
+
+export default function CreateReviewPage() {
+  return (
+    <div className={styles.container}>
+      <h2>Создать отзыв</h2>
+      <p>Страница временно находится на реконструкции.</p>
+    </div>
+  );
+} 
