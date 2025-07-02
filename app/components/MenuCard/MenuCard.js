b91e7e9e2a5b596afea5cda04df0ff9985535162
MELEGHOST <kosbek043@gmail.com>
2025-07-02 21:15:55 +0300

    fix: Удалены комментарии из компонента MenuCard для улучшения читаемости кода и упрощения структуры. Обновлены стили для более четкого отображения элементов.

--- app/components/MenuCard/MenuCard.js
+++ app/components/MenuCard/MenuCard.js
@@ -7,13 +7,10 @@ const MenuCard = ({ href, icon, label, description, isActive, onPointerEnter }) 
   return (
     <li className={styles.listItem} data-active={isActive} onPointerEnter={onPointerEnter}>
       <Link href={href} className={styles.linkWrapper}>
-        {/* Collapsed View */}
         <div className={styles.collapsedView}>
           <div className={styles.icon}>{icon}</div>
           <h3 className={styles.labelVertical}>{label}</h3>
         </div>
-
-        {/* Expanded View */}
         <div className={styles.expandedView}>
           <h4 className={styles.labelHorizontal}>{label}</h4>
           <p className={styles.description}>{description}</p>
