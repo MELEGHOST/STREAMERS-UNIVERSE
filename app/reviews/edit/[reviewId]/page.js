4374815383c4a1c23d67dd25f9ece7ce221c07ca
MELEGHOST <kosbek043@gmail.com>
2025-07-03 20:33:47 +0300

    fix: Исправлен путь импорта компонента AuthContext в странице редактирования отзыва для корректной работы приложения.

--- app/reviews/edit/[reviewId]/page.js
+++ app/reviews/edit/[reviewId]/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter, useParams } from 'next/navigation';
-import { useAuth } from '../../../../contexts/AuthContext'; // Путь вроде верный
+import { useAuth } from '../../../contexts/AuthContext'; // Путь вроде верный
 import styles from '../edit-review.module.css'; // ИСПРАВЛЕННЫЙ ПУТЬ
 import pageStyles from '../../../../styles/page.module.css'; // Путь вроде верный
 import { useTranslation } from 'react-i18next';
