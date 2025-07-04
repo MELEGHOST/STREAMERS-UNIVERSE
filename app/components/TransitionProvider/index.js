a77614769d11583eb308b1843349228b070eba91
MELEGHOST <kosbek043@gmail.com>
2025-07-04 21:59:07 +0300

    fix: Упрощен компонент TransitionProvider, удалены ненужные состояния и обработчики анимации, улучшены визуальные эффекты переходов.

--- app/components/TransitionProvider/index.js
+++ app/components/TransitionProvider/index.js
@@ -2,59 +2,29 @@
 
 import { motion, AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
-import Loader from '../Loader/Loader';
 
 const TransitionProvider = ({ children }) => {
     const pathname = usePathname();
-    const [isTransitioning, setIsTransitioning] = useState(false);
 
     const variants = {
-        hidden: {
-            opacity: 0,
-            scale: 1.1,
-            transition: { duration: 0.5, ease: 'easeInOut' }
-        },
-        visible: {
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, ease: 'easeInOut' }
-        },
-        exit: {
-            opacity: 0,
-            scale: 0.1,
-            transition: { duration: 0.7, ease: 'easeIn' }
-        }
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+        exit: { opacity: 0 },
     };
 
     return (
-        <>
-            <AnimatePresence>
-                {isTransitioning && <Loader />}
-            </AnimatePresence>
-            
-            <AnimatePresence mode="wait">
-                <motion.div
-                    key={pathname}
-                    initial="hidden"
-                    animate="visible"
-                    exit="exit"
-                    variants={variants}
-                    onAnimationStart={definition => {
-                        if (definition === 'exit') {
-                           setIsTransitioning(true);
-                        }
-                    }}
-                    onAnimationComplete={definition => {
-                        if (definition === 'visible') {
-                            setIsTransitioning(false);
-                        }
-                    }}
-                >
-                    {children}
-                </motion.div>
-            </AnimatePresence>
-        </>
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={pathname}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                variants={variants}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
