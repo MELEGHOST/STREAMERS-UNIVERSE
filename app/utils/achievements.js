98a0f437c3bd79b8fbebcd5d62023884910b3ce6
MELEGHOST <kosbek043@gmail.com>
2025-07-18 19:10:39 +0300

    fix: Удалены неиспользуемые переменные для инициализации Supabase клиента в файле achievements.js, что улучшает читаемость кода и снижает вероятность ошибок.

--- app/utils/achievements.js
+++ app/utils/achievements.js
@@ -1,9 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
-const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
-
 // Функция для получения количества обзоров пользователя
 async function getUserReviewCount(supabaseAdmin, userId) {
   const { count, error } = await supabaseAdmin
