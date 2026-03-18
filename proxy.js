80a459ab644241e338ceac994da6ffac8c7c4fee
MELEGHOST <kosbek043@gmail.com>
2026-03-18 03:37:36 +0300

    co

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { createServerClient } from '@supabase/ssr';
 
-export async function middleware(req) {
+export async function proxy(req) {
   let res = NextResponse.next({
     request: {
       headers: req.headers,
